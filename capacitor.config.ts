024e1af417983254bb4f406a71c1c13c9e6eefb5
Muthus <nakkubetta.app@gmail.com>
2022-12-30 17:12:50 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uk.nbtv.v2',
+  appName: 'nakkubettaTV',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
